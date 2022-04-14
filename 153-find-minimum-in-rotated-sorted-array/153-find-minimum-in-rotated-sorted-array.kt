84ad59afe38e0fb2caf0431bcb0b441ccb50ab7b
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-04-14 15:05:17 +0900

    Time: 171 ms (91.06%), Space: 36.4 MB (48.94%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.kt
@@ -0,0 +1,40 @@
+class Solution {
+    
+    lateinit var nums: IntArray
+    var N = 0
+    
+    fun findMin(nums: IntArray): Int {
+        
+        this.nums = nums
+        this.N = nums.size
+        var start = 0
+        var end = N
+        var mid: Int
+        
+        while(start<end){
+            mid = (start+end)/2
+            if(isMin(start))
+                return nums[start]
+            if(start!=mid && isMin(mid))
+                return nums[mid]
+            if(nums[start]>nums[mid]){
+                end = mid-1
+            }
+            else{
+                start = mid+1
+            }
+        }
+        return nums[end.coerceAtMost(N-1)]
+    }
+    
+    private fun isMin(idx: Int): Boolean {
+        val before = (idx+N-1)%N
+        val after = (idx+N+1)%N
+        
+        if(nums[idx]<nums[before] && nums[idx]<nums[after])
+            return true
+        return false
+    }
+    
+    
+}
