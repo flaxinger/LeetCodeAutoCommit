2d390064cc0c9c87e9e88029e3395596d9971237
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-05-27 13:37:15 +0900

    Time: 217 ms (25.69%), Space: 34 MB (5.50%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.kt
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.kt
@@ -1,15 +1,15 @@
 class Solution {
     
     fun numberOfSteps(num: Int): Int {
-        var arr = Array(num+1, {i-> i})
-        for(i in 0..num){
-            if(i*2 <= num){
-                arr[i*2] = Math.min(arr[i]+1, arr[i*2])
-            }
-            if(i < num){
-                arr[i+1] = Math.min(arr[i]+1, arr[i+1])
-            }
+        var ans = 0
+        var cur = num
+        while(cur > 0){
+            if(cur%2 == 0)
+                cur/=2
+            else
+                cur--
+            ans++
         }
-        return arr[num]
+        return ans
     }
 }
