6c30a5a206e92c240d442cb80b3bfebacf285588
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-05-28 15:14:29 +0900

    Time: 412 ms (47.53%), Space: 52.3 MB (34.03%) - LeetHub

--- 268-missing-number/268-missing-number.kt
+++ 268-missing-number/268-missing-number.kt
@@ -1,9 +1,8 @@
 class Solution {
     fun missingNumber(nums: IntArray): Int {
-        var sum = nums.sum()
         var target = 0
         for(i in 0..nums.size)
             target+=i
-        return target - sum
+        return target - nums.sum()
     }
 }
