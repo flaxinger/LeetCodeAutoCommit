63309de37374b8b91cc3ab1bc7adfef841dfdea0
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-05-31 21:30:28 +0900

    Time: 238 ms (33.33%), Space: 34 MB (30.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.kt
+++ 191-number-of-1-bits/191-number-of-1-bits.kt
@@ -2,8 +2,9 @@ class Solution {
     // you need treat n as an unsigned value
     fun hammingWeight(n:Int):Int {
         var cnt = 0
+        var N = n.toLong()
         for(i in 0 until 32){
-            if(n.toLong() and (1L shl i) > 0)
+            if(N and (1L shl i) > 0)
                 cnt++
         }
         return cnt
