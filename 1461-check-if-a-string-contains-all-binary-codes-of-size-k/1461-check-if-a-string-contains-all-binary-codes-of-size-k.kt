8e2a218914defd02b0e3256a508dfcf666847f88
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-05-31 21:13:20 +0900

    Time: 563 ms (100.00%), Space: 55 MB (100.00%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.kt
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.kt
@@ -3,7 +3,6 @@ class Solution {
     val set = mutableSetOf<String>()
     
     fun hasAllCodes(s: String, k: Int): Boolean {
-        var ss = StringBuilder(s.substring(0, Math.min(k, s.length)))
         for(i in 0..s.length-k){
             set.add(s.substring(i, i+k))
         }
