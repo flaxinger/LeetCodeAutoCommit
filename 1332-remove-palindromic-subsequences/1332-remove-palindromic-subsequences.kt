635abe625bfddeb645f3fd981176fca666e2601c
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-06-08 12:50:22 +0900

    Time: 237 ms (16.67%), Space: 35.3 MB (50.00%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.kt
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.kt
@@ -1,29 +1,6 @@
 class Solution {
       
     fun removePalindromeSub(s: String): Int {
-        if(findAndRemovePalindrome(StringBuilder(s)) == "") return 1
-        return 2
-    }
-    
-    fun findAndRemovePalindrome(s: StringBuilder): String{
-        var start = 0
-        var end = s.length-1
-        while(start<=end){
-            if(s[start]==s[end]){
-                if(start!=end){
-                    s.deleteCharAt(end)
-                    s.deleteCharAt(start)
-                }
-                else{
-                    s.deleteCharAt(start)
-                    end = s.length-1
-                }
-                end-=2
-            }
-            else{
-                end--
-            }
-        }
-        return s.toString()
+        return if(s == s.reversed()) 1 else 2 
     }
 }
