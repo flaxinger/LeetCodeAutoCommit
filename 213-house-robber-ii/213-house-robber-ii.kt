03d1de7244567636cf437f0f3ab9b4762609b33c
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-06-09 15:52:53 +0900

    Time: 156 ms (95.60%), Space: 33.7 MB (93.71%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        val N = nums.size
+        val dp = Array(2) {IntArray(N) {-1}}
+        if(N == 1) return nums[0]
+        dp[0][0] = nums[0]
+        dp[0][1] = Math.max(nums[1], nums[0])
+        dp[1][0] = 0
+        dp[1][1] = nums[1] 
+        for(i in 2 until N-1){
+            dp[0][i] = Math.max(nums[i]+dp[0][i-2], dp[0][i-1])
+        }
+        for(i in 2 until N){
+            dp[1][i] = Math.max(nums[i]+dp[1][i-2], dp[1][i-1])
+        }
+        //print
+        // for(arr in dp){
+        //     for(a in arr){
+        //         print("$a, ")
+        //     }
+        //     println()
+        // }
+        // println()
+        return Math.max(dp[0][N-2], dp[1][N-1])
+    }
+}
